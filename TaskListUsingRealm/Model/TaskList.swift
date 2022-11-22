d821c9f5256468e4670552da9334c3a75cb600e7
Александр Биктеев <bikteev@mail.ru>
2022-11-22 20:36:12 +0600

    Setup UI Setup Model Setup StorageManager

---
+++ TaskListUsingRealm/Model/TaskList.swift
@@ -0,0 +1,22 @@
+//
+//  TaskList.swift
+//  TaskListUsingRealm
+//
+//  Created by albik on 22.11.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
